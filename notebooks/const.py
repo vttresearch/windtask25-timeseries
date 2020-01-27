81988964f6bef95d001b4b5935557a04d53ba091
Erkka Rinne <erkka.rinne@vtt.fi>
2020-01-27 13:32:25 +0200

    First set of notebooks with wind power CF calculation.
    Too high values for Belgian Offshore, still unresolved.

---
+++ notebooks/const.py
@@ -0,0 +1,29 @@
+"""This module provides constant settings
+"""
+
+TZ = 'UTC'
+
+START = "20180101"
+END = "20190101"
+
+COUNTRIES = [
+  # Task 25 countries in Europe
+ 'Denmark',
+ 'Finland',
+ 'France',
+ 'Germany',
+ 'Ireland',
+ 'Italy',
+ 'Netherlands',
+ 'Norway',
+ 'Portugal',
+ 'Spain',
+ 'Sweden',
+ 'United Kingdom',
+ # Others
+ 'Belgium',
+ 'Austria']
+
+GEN_TYPES = ["Solar", "Wind Onshore", "Wind Offshore"]
+
+MIN_TIMESTEP = '15 min'
